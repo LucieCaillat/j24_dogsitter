bab8178aaa800b7090473a1e502adea96fe2ed51
Lucie Caillat <lucie.caillat@gmail.com>
2021-10-28 16:54:27 +0200

    FINAL commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+City.destroy_all
+Dog.destroy_all
+Dogsitter.destroy_all
+Stroll.destroy_all
+
+# 9 villes
+ cities_array = [{city_name: "PARIS" , zip_code: "75000"}, {city_name: "TOULOUSE" , zip_code: "31000"}, {city_name: "NICE" , zip_code: "06000"}, {city_name: "NANTES" , zip_code: "44000"}, {city_name: "STRASBOURG" , zip_code: "67000"}, {city_name: "MONPELLIER" , zip_code: "34000"}, {city_name: "BORDEAUX" , zip_code: "33000"}, {city_name: "LILLE" , zip_code: "59000"}, {city_name: "RENNES" , zip_code: "35000"}]
+
+ cities_array.each_with_index do |city_to_save, index|
+     City.create(city_to_save)
+     puts "#{index} city créées"
+ end
+
+ 100.times do 
+     Dog.create(dog_name: Faker::Games::Pokemon.name , city: City.all[rand(0..8)] )
+     puts "waf"
+ end
+
+ 100.times do 
+     Dogsitter.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name , city: City.all[rand(0..8)])
+     puts "hello"
+ end
+
+100.times do 
+    Stroll.create(dogsitter: Dogsitter.all[rand(0..99)], dog: Dog.all[rand(0..99)])
+    puts "je me balade"
+end
